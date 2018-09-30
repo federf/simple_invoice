efb7cf5f88ee9880aec65ff905ed69ce971bcb4e
federf <fede051993@gmail.com>
2018-09-30 14:19:47 -0300

    permitir cambiar fecha nueva factura y editar factura, actualizar pdf

--- pdf/documentos/factura_pdf.php
+++ pdf/documentos/factura_pdf.php
@@ -16,9 +16,9 @@
 	include("../../config/conexion.php");
 	//Archivo de funciones PHP
 	include("../../funciones.php");
-	$session_id= session_id();
-	$sql_count=mysqli_query($con,"select * from tmp where session_id='".$session_id."'");
-	$count=mysqli_num_rows($sql_count);
+	$session_id = session_id();
+	$sql_count  = mysqli_query($con,"select * from tmp where session_id='".$session_id."'");
+	$count      = mysqli_num_rows($sql_count);
 	if ($count==0)
 	{
         echo "<script>alert('No hay productos agregados a la factura')</script>";
@@ -29,15 +29,16 @@
 	require_once(dirname(__FILE__).'/../html2pdf.class.php');
 
 	//Variables por GET
-	$id_cliente=intval($_GET['id_cliente']);
-	$id_vendedor=intval($_GET['id_vendedor']);
-	$condiciones=mysqli_real_escape_string($con,(strip_tags($_REQUEST['condiciones'], ENT_QUOTES)));
+	$id_cliente    = intval($_GET['id_cliente']);
+	$id_vendedor   = intval($_GET['id_vendedor']);
+	$fecha_factura = date("Y-m-d H:i:s", strtotime(str_replace('/', '-', $_GET['fecha'])));
+    $condiciones   = mysqli_real_escape_string($con,(strip_tags($_REQUEST['condiciones'], ENT_QUOTES)));
 
 	//Fin de variables por GET
-	$sql=mysqli_query($con, "select LAST_INSERT_ID(numero_factura) as last from facturas order by id_factura desc limit 0,1 ");
-	$rw=mysqli_fetch_array($sql);
-	$numero_factura=$rw['last']+1;
-	$simbolo_moneda=get_row('perfil','moneda', 'id_perfil', 1);
+	$sql            = mysqli_query($con, "select LAST_INSERT_ID(numero_factura) as last from facturas order by id_factura desc limit 0,1 ");
+	$rw             = mysqli_fetch_array($sql);
+	$numero_factura = $rw['last']+1;
+	$simbolo_moneda = get_row('perfil','moneda', 'id_perfil', 1);
     // get the HTML
     ob_start();
     include(dirname('__FILE__').'/res/factura_html.php');
